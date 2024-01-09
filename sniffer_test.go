f7e04cedb82c3a3a14b298505dbf2c667ecbed69
Michalis Papamichael <michalis.papamichael@outlook.com>
2024-01-09 21:24:15 +0200

    Minor changes in testing

--- sniffer_test.go
+++ sniffer_test.go
@@ -6,12 +6,14 @@ import (
 	"time"
 
 	"github.com/google/gopacket/pcap"
+	"github.com/hashicorp/go-hclog"
 )
 
 func TestSniffPackets(t *testing.T) {
-	iname := "wlp2s0"
-	filter := ""
-	s := Sniffer{InterfaceName: &iname, BpfFilterExpr: &filter,
+	l := hclog.Default()
+	// iname := "wlp2s0"
+	filter := "tcp"
+	s := Sniffer{InterfaceName: /*&iname*/ nil, BpfFilterExpr: &filter,
 		SnapshotLength: 1024, Duration: pcap.BlockForever, Promiscuous: false}
 	pkts, err := s.StartSniff()
 	if err != nil {
@@ -23,9 +25,11 @@ func TestSniffPackets(t *testing.T) {
 			// do something
 			fmt.Println(p)
 		}
-		fmt.Println("Packets Channel Closed")
+		l.Info("Packets channel closed")
 	}()
-	<-time.After(10 * time.Second)
-	s.Close()
+	<-time.After(15 * time.Second)
+	l.Info("Closing packet sniffer")
+	stats := s.Close(true)
+	fmt.Printf("\n Stats %v\n", stats)
 	<-time.After(10 * time.Second)
 }
