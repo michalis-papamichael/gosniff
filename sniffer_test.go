9bcbe1851366227dd2ad713b35c27367b9408896
Michalis Papamichael <michalis.papamichael@outlook.com>
2024-01-10 19:06:43 +0200

    Slight naming changes in test

--- sniffer_test.go
+++ sniffer_test.go
@@ -10,12 +10,11 @@ import (
 )
 
 func TestSniffPackets(t *testing.T) {
-	l := hclog.Default()
-	// iname := "wlp2s0"
+	log := hclog.Default()
 	filter := "tcp"
-	s := Sniffer{InterfaceName: /*&iname*/ nil, BpfFilterExpr: &filter,
+	sniffer := Sniffer{InterfaceName: nil, BpfFilterExpr: &filter,
 		SnapshotLength: 1024, Duration: pcap.BlockForever, Promiscuous: false}
-	pkts, err := s.StartSniff()
+	pkts, err := sniffer.StartSniff()
 	if err != nil {
 		t.Fatal(err)
 		panic(err)
@@ -25,11 +24,11 @@ func TestSniffPackets(t *testing.T) {
 			// do something
 			fmt.Println(p)
 		}
-		l.Info("Packets channel closed")
+		log.Info("Packets channel closed")
 	}()
 	<-time.After(15 * time.Second)
-	l.Info("Closing packet sniffer")
-	stats := s.Close(true)
-	fmt.Printf("\n Stats %v\n", stats)
+	log.Info("Closing packet sniffer")
+	stats, _ := sniffer.CloseAndGetStats(false)
+	fmt.Printf("\n Stats Received: %v\n", stats.PacketsReceived)
 	<-time.After(10 * time.Second)
 }
